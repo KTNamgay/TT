75b18ceeafad28454fb99c8d133929f86fc6f723
KTNamgay <ktnamgay18@gmail.com>
2022-11-21 22:59:37 +0600

    mobile

---
+++ script.js
@@ -0,0 +1,46 @@
+function chat() {
+    alert("Druk Juthin Na Jam Ba lak so");
+    const q1= prompt("How Can I Help You?");
+     if (q1=="data chi zhu gay la") { 
+     alert("laso laso la");
+     }
+    else if (q1=="network issue") {
+    alert("call 1600 1600 for further inquiry");
+    } 
+    else if (q1=="no") {
+    alert("Bmobile, Keep in Touch");
+    }
+    else { 
+    alert("Call 1600");
+    }
+    const q2= prompt("What Do You Want?");
+    if (q2=="data") { 
+alert("Okay");
+    }
+    
+    const q3= prompt("HOW MUCH DO YOU WANT?");
+    if (q3=="99") {
+   alert("Check Your Mobile Balance");
+    }
+      else if (q3=="199") {
+   alert("Check Your Mobile Balance");
+    }
+      else if (q3=="299") {
+   alert("Check Your Mobile Balance");
+    }
+      else if (q3=="499") {
+   alert("Check Your Mobile Balance");
+    }
+      else if (q3=="699") {
+   alert("Check Your Mobile Balance");
+    }
+      else if (q3=="999") {
+   alert("Check Your Mobile Balance");
+    }
+      else if (q3=="1299") {
+   alert("Check Your Mobile Balance");
+    }
+    else { 
+    alert("Check Your Mobile Balance");
+    }
+}
